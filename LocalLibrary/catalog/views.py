e1a0aa1affc8d95a30f6cd01c8feb7d7e4959970
gr402_hudal <dimahudobin05@gmail.com>
2022-09-16 18:34:37 +0700

    16/09/22

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     # Генерация "количеств" некоторых главных объектов
@@ -8,11 +9,27 @@ def index(request):
     # Доступные книги (стаус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genre=Genre.objects.count()
+    num_i_book = Book.objects.filter(title__icontains='ье').count()
+
+    context = {'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'num_authors': num_authors,
+               'num_genre': num_genre,
+               'num_i_book': num_i_book}
 
     # Отрисовка HTML-шаблона base_generic.html с данными внутри
     # переменной контекста context
     return render(
         request,
-        'base_generic.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        'index.html',
+        context=context,
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
