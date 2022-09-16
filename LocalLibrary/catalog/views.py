2ad78769241254c4309346e3f6436ac73faba768
ADM <dimahudobin05@gmail.com>
2022-09-16 13:36:26 +0700

    16.09

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -9,10 +9,10 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
+    # Отрисовка HTML-шаблона base_generic.html с данными внутри
     # переменной контекста context
     return render(
         request,
-        'index.html',
+        'base_generic.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
