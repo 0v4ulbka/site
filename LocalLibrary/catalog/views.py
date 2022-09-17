56b388f26f6d9923dee8d80db9e8d0ef2494a694
gr402_hudal <dimahudobin05@gmail.com>
2022-09-17 11:30:19 +0700

    17/09/22

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -12,12 +12,16 @@ def index(request):
     num_genre=Genre.objects.count()
     num_i_book = Book.objects.filter(title__icontains='ье').count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {'num_books': num_books,
                'num_instances': num_instances,
                'num_instances_available': num_instances_available,
                'num_authors': num_authors,
                'num_genre': num_genre,
-               'num_i_book': num_i_book}
+               'num_i_book': num_i_book,
+               'num_visits':num_visits}
 
     # Отрисовка HTML-шаблона base_generic.html с данными внутри
     # переменной контекста context
@@ -33,3 +37,9 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
